3187248fc9d50466ef17d8e851f5cdb89247791a
nilendughosal-sym <nilendughosal@symphony.com>
2022-09-03 11:24:10 +0530

    stop tracking dataset/split

---
+++ src/train_test_split.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 23:39:09 2022
+
+@author: 703296118NG
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import plac
+
+
+from sklearn.model_selection import train_test_split
+
+
+def main(data_path='../dataset/Crop_recommendation.csv' , out_path='../dataset/split'):
+    df=pd.read_csv(data_path)
+    train, test = train_test_split(df,random_state=1)
+
+    if not os.path.isdir(out_path):
+        os.mkdir(out_path)
+    train.to_csv(f'{out_path}/train.csv' , index=False)
+    test.to_csv(f'{out_path}/test.csv' , index=False)
+    print("Split data set finished sucessfully")
+
+if __name__=='__main__':
+    plac.call(main)

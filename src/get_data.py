85308d94f24a8c4de2e3b8a9986962002a2d2541
nilendughosal-sym <nilendughosal@symphony.com>
2022-09-06 21:21:56 +0530

    Test

--- src/get_data.py
+++ src/get_data.py
@@ -10,13 +10,13 @@ import plac
     out_path=("Path to save split data" , "option" , "o" , str)
 )
 
-def main(data_path='crop_input_data2.csv' , out_path='dataset/Crop_recommendation.csv'):
+def main(data_path='crop_input_data.csv' , out_path='dataset'):
     ACCESS_KEY = 'AKIAWATRK4TZJTQ4BA2I'
     SECRET_KEY = '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'
     
 
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
-    s3.download_file('enq-dataops-pipeline-artifacts', f'{data_path}', f'{out_path}')
+    s3.download_file('enq-dataops-pipeline-artifacts', f'{data_path}', f'{out_path}/Crop_recommendation.csv')
 
     print("Data fetch from S3 sucessfull")
 

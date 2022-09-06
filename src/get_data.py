297c18d2e1a5639e1cc018399ff56efa16eacf61
nilendughosal-sym <nilendughosal@symphony.com>
2022-09-06 15:14:12 +0530

    Check Pipeline

--- src/get_data.py
+++ src/get_data.py
@@ -5,13 +5,18 @@ import os
 import plac
 
 
+@plac.annotations(
+    data_path =("Path to source data" , "option" , "i" , str),
+    out_path=("Path to save split data" , "option" , "o" , str)
+)
+
 def main(data_path='crop_input_data.csv' , out_path='dataset/Crop_recommendation.csv'):
     ACCESS_KEY = 'AKIAWATRK4TZJTQ4BA2I'
     SECRET_KEY = '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'
     
 
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY , aws_secret_access_key=SECRET_KEY)
-    s3.download_file('enq-dataops-pipeline-artifacts', 'Crop_input_data2.csv', 'dataset/Crop_recommendation.csv')
+    s3.download_file('enq-dataops-pipeline-artifacts', f'{data_path}', f'{out_path}')
 
     print("Data fetch from S3 sucessfull")
 
